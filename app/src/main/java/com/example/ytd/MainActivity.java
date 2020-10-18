ddf5c7eb4203acdb4a6db0e67a8898d2c54369a0
Tulasi <tulasireddykagithala@gmail.com>
2020-10-18 21:17:07 +0530

    Second Commit

--- app/src/main/java/com/example/ytd/MainActivity.java
+++ app/src/main/java/com/example/ytd/MainActivity.java
@@ -19,6 +19,8 @@ import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
@@ -53,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
     private String url;
-    private ProgressDialog progressDialog;
+    private ProgressBar progressBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,53 +65,30 @@ public class MainActivity extends AppCompatActivity {
         AndroidNetworking.initialize(getApplicationContext());
         editText = findViewById(R.id.edit_text);
         button = findViewById(R.id.download);
-        progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setMessage("Downloading Video, Please wait");
-        progressDialog.setMax(100);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        String youtubeLink = "https://www.youtube.com/watch?v=oka9Vzlja-4";
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.percent);
+        progressBar.setMax(100);
+        progressBar.setProgress(0);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                url = editText.getText().toString().trim();
-//                if (url != null && !url.isEmpty()){
-//
-//                    if(url.contains("https://www.youtube.com/watch?v=") || url.contains("https://youtu.be/")){
-//                        CheckPermission();
-//                    }else {
-//                        Toast.makeText(MainActivity.this," Enter Only YouTube Url", Toast.LENGTH_SHORT).show();
-//                    }
-//
-//                }else {
-//                    Toast.makeText(MainActivity.this," Enter YouTube Url", Toast.LENGTH_SHORT).show();
-//                }
-CheckPermission();
+                url = editText.getText().toString().trim();
+                if (!url.isEmpty()){
+
+                    if(url.contains("https://www.youtube.com/watch?v=") || url.contains("https://youtu.be/")){
+                        CheckPermission();
+                    }else {
+                        Toast.makeText(MainActivity.this," Enter Only YouTube Url", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(MainActivity.this," Enter YouTube Url", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-//        new YouTubeExtractor(this) {
-//            @Override
-//            public void onExtractionComplete(SparseArray<YtFile> ytFiles, VideoMeta vMeta) {
-//                if (ytFiles != null) {
-//                    int itag = 22;
-//                    String downloadUrl = ytFiles.get(itag).getUrl();
-//                    Log.d("yt", downloadUrl);
-//                    Toast.makeText(MainActivity.this, downloadUrl, Toast.LENGTH_LONG).show();
-//
-//                    if (downloadUrl != null) {
-//                        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-//                        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadUrl));
-//                        request.setTitle(ytFiles.ge);
-//                        request.setDestinationInExternalPublicDir("/Downloads/YouTube-Downloader/", videoTitle + ".mp4");
-//                        if (downloadManager != null) {
-//                            downloadManager.enqueue(request);
-//                        }
-//                    }
-//                } else Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        }.extract(youtubeLink, true, true);
+
     }
 
     private void CheckPermission() {
@@ -119,17 +99,9 @@ CheckPermission();
                     public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
 
                        if(multiplePermissionsReport.areAllPermissionsGranted()){
+                           button.setEnabled(false);
                            YTDownload(18);
-//                           File myDirectory = new File(Environment.getExternalStorageDirectory(), "Movies");
-//
-//                           if(myDirectory.exists()) {
-//                               Toast.makeText(MainActivity.this, "No Folder", Toast.LENGTH_SHORT).show();
-//                               Log.e("dirs", "No Folder");
-//                           }else{
-//                               myDirectory.mkdirs();
-//                               Toast.makeText(MainActivity.this, myDirectory.toString(), Toast.LENGTH_SHORT).show();
-//                               Log.e("dirs", myDirectory.toString());
-//                           }
+
                        }else if(multiplePermissionsReport.isAnyPermissionPermanentlyDenied()){
                            MultiplePermissionsListener dialogMultiplePermissionsListener =
                                    DialogOnAnyDeniedMultiplePermissionsListener.Builder
@@ -151,44 +123,9 @@ CheckPermission();
                 }).check();
     }
 
-    private void CheckPermissions() {
-        Dexter.withActivity(MainActivity.this)
-                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                       // YTDownload(18 );
-                    }
 
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse response) {
-                        if(response.isPermanentlyDenied()){
-                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                            builder.setTitle("Permision Required")
-                                    .setMessage("Permission Required to access image")
-                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            Intent intent = new Intent();
-                                            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                            intent.setData(Uri.fromParts("pakage", getPackageName(), null));
-                                            startActivityForResult(intent, 51);
-                                        }
-                                    })
-                                    .setNegativeButton("Cancel",null)
-                                    .show();
-                        }
-                    }
-
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
-                    }
-                }).check();
-    }
 
     public void YTDownload(final int itag) {
-       // String VideoURLDownload = "https://www.youtube.com/watch?v=oka9Vzlja-4";
         String VideoURLDownload = editText.getText().toString().trim();
         @SuppressLint("StaticFieldLeak") YouTubeUriExtractor youTubeUriExtractor = new YouTubeUriExtractor(this) {
             @Override
@@ -198,17 +135,8 @@ CheckPermission();
                   //  Log.e("Download URL:", downloadURL);
 
                     if (downloadURL != null) {
-//                        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-//                        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadURL));
-//                        request.setTitle(videoTitle);
-//                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_MOVIES, videoTitle + ".mp4");
-//                        if (downloadManager != null) {
-//                            downloadManager.enqueue(request);
-//                        }
-
-                       // DownloadManager2(downloadURL, videoTitle);
+                        button.setEnabled(false);
                         Download_video(downloadURL, videoTitle);
-
                     }
                 } else Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
             }
@@ -216,47 +144,24 @@ CheckPermission();
         youTubeUriExtractor.execute(VideoURLDownload);
     }
 
-    private void DownloadManager2(final String uri, final String filename) {
-        DownloadManager downloadManager;
-        downloadManager = (DownloadManager) getSystemService(getApplicationContext().DOWNLOAD_SERVICE);
-        Uri urls = Uri.parse(uri);
-        DownloadManager.Request request = new DownloadManager.Request(urls);
-        request.setAllowedNetworkTypes(
-                DownloadManager.Request.NETWORK_WIFI
-                        | DownloadManager.Request.NETWORK_MOBILE)
-                .setAllowedOverRoaming(false).setTitle(filename)
-                .setDescription("Something useful. No, really.")  // "ImageName.jpg"
-                .setDestinationInExternalPublicDir(Environment.DIRECTORY_MOVIES, filename + ".mp4")
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        downloadManager.enqueue(request);
-    }
+
 
     private void Download_video(final String uri, final String filename){
-        progressDialog.show();
+//        progressDialog.show();
         Log.e("Download URL:", uri);
-        File dirpath = new File(Environment.getExternalStorageDirectory().getPath()
-                + "/MyVideos");
-        if (!dirpath.exists()) {
-            dirpath.mkdirs();
-        }
-
 
         File myDirectory = new File(Environment.getExternalStorageDirectory(), "Movies12");
+        if(myDirectory.exists()) {
+            Toast.makeText(MainActivity.this, "No Folder", Toast.LENGTH_SHORT).show();
+            Log.e("dirs", "No Folder");
+        }else{
+            myDirectory.mkdirs();
+            Toast.makeText(MainActivity.this, myDirectory.toString(), Toast.LENGTH_SHORT).show();
+            Log.e("dirs", myDirectory.toString());
+        }
 
-                           if(myDirectory.exists()) {
-                               Toast.makeText(MainActivity.this, "No Folder", Toast.LENGTH_SHORT).show();
-                               Log.e("dirs", "No Folder");
-                           }else{
-                               myDirectory.mkdirs();
-                               Toast.makeText(MainActivity.this, myDirectory.toString(), Toast.LENGTH_SHORT).show();
-                               Log.e("dirs", myDirectory.toString());
-                           }
+        Log.e("dir", myDirectory.toString());
 
-
-
-        Log.e("dir", dirpath.toString());
-
-//        File dirpath = new File(Environment.DIRECTORY_MOVIES);
         final File file = new File(myDirectory, filename);
         AndroidNetworking.download(uri, myDirectory.toString(), filename+".mp4")
                 .setTag("downloadTest")
@@ -267,13 +172,14 @@ CheckPermission();
                     public void onProgress(long bytesDownloaded, long totalBytes) {
                         // do anything with progress
                         float progress = (float) bytesDownloaded / totalBytes * 100;
-                        progressDialog.setProgress((int)progress);
+                        progressBar.setProgress((int)progress);
+                        textView.setText(""+(int)progress+"%");
                     }
                 })
                 .startDownload(new DownloadListener() {
                     @Override
                     public void onDownloadComplete() {
-                        progressDialog.dismiss();
+                        button.setEnabled(true);
                         Toast.makeText(getApplicationContext(), filename + " Downloaded", Toast.LENGTH_SHORT).show();
                         Log.d("Tag", "Scan finished. You can view the image in the DDDDDDD now.");
                         try {
@@ -295,7 +201,7 @@ CheckPermission();
                         // handle error
                         Toast.makeText(getApplicationContext(), "Error: " + error, Toast.LENGTH_SHORT).show();
                         Log.e("e", error.toString());
-                        progressDialog.dismiss();
+                        button.setEnabled(true);
                     }
                 }); // === end download ===
     }
